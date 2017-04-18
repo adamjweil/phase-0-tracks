c3d2563befe79f513cb313694e896fa3ee9efb4a
Adam Weil <adamjweil@gmail.com>
2017-04-17 22:28:28 -0400

    Updating vampire ruby project

---
+++ ruby/vampires.rb
@@ -0,0 +1,16 @@
+
+puts "What is your name? "
+name = gets
+
+puts "How old are you? "
+age = gets
+
+puts "What year were you born? "
+year = gets
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? "
+garlic = gets
+
+puts "Would you like to enroll in the company’s health insurance? "
+insurance = gets
+
