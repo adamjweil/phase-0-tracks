a8ed79b4effb448c693d51cf129e3473f6544ef0
Adam Weil <adamjweil@gmail.com>
2017-04-20 20:39:30 -0400

    Finalized my secret agents program

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -6,19 +6,19 @@
 #   -Make a counter starting at zero
 #   Algorithm
     # -Create a loop that executes until the counter variable is  equal to the string length
-      # -For each index in the loop, identify the next character in the alphabet by using the .next method
-      # -Add this .next value to our password variable
+      # -For each index in the loop, identify the next character in the alphabet
+      # -Add this value to our password variable
     # -After the loop is complete, return the password in reverse.
 # Example input: "Abc"
 # Example output: "Bbc"
 
 # ----> Decrypt Method
-  # input: recieve a string
-  # make a counter starting at 0
-  # define an empty variable for decrypted password
+  # Input: Receive a string
+  #  -Make a counter starting at 0
+  #  -Define an empty variable for decrypted password
   # Algorithm:
-    # create a loop that executes until the counter equals the length of string
-      # have a variable with entire alphabet (alphabet)
+    # -Create a loop that executes until the counter equals the length of string
+      # Have a variable with entire alphabet (alphabet)
       # for each letter in the string, find the index of the letter in the alphabet, and store in a variable
       # use the letter variable as the index in the alphabet, and add the result to password
       # Add 1 to the counter
@@ -26,22 +26,19 @@
   # example input: "bcd"
   # example output: "abc"
 
-# Advances every letter of a string one letter forward, Encrypting it
-def encrypt(string)
-  counter = 0
-  password = ""
+def encrypt(pass)
+   counter = 0
+   password = ""
 
-  until counter == string.length
-      if string[counter] == 'z'
-        password = 'a'
-    else
-      password = string[counter].next + password
-      counter +=1
-      end
-    end
-password.reverse
-end
+   until counter == pass.length
+     alphabet = "abcdefghijklmnopqrstuvwxyza"
+     letter = alphabet.index(pass[counter]) + 1
+     counter += 1
+     password = alphabet[letter] + password
 
+   end
+   password.reverse
+ end
 
 # Reverses the method above, Decrypting a password
  def decrypt(word)
@@ -63,15 +60,16 @@ end
 #OUR DRIVER CODE
 
 # ---->(Release 3) TEST CASES <----
-# encrypt("abc")
-# encrypt("zed")
-# decrypt("bcd")
-# decrypt("afe")
+# puts encrypt("abc")
+# puts encrypt("zed")
+# puts decrypt("bcd")
+# puts decrypt("afe")
 
 # ---->(Release 4) TRY A NESTED METHOD CALL <----
-puts decrypt(encrypt("swordfish"))
+# puts decrypt(encrypt("swordfish"))
 # ------> Why does "decrypt(encrypt("swordfish"))" work <-----
-# Answer: This nested method works because the return value of
+
+# Answer: This nested method works because the return value (output) of encrypt method is a string value, and the input required for the decrypt method is also a string value. If the return value for the encrypt method was an integer, then this nested method wouldn't work, as decrypt method requires a string as an argument.
 
            # ---- >INTERFACE<-----
 # asks whether they want to encrypt or decrypt a password
@@ -93,6 +91,7 @@ end
 
 # Returns encrypted / decrypted password
 puts "Returns: #{answer}"
+
 
 
 
