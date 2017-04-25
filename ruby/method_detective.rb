7e184e62f95c34601f2574dbabd6d6216eb425e4
Adam Weil <adamjweil@gmail.com>
2017-04-25 01:50:25 -0400

    Identified methods being used for assignment 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,42 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+=> “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(2, 'o')
+=> “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center(15)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+"the usual".insert(-1, ' suspects')
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".insert(0, 'The Usual')
+=> "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+"The case of the disappearing last letter".chop
+=> "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+"The mystery of the missing first letter".delete "T"
+=> "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+"Elementary,    my   dear        Watson!".squeeze(" ")
+=> "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
+"z".ord
+=> 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# "a".ord produces the value 97. The .ord method returns the integer orderinal of a 
+# one-character string. Ordinals number is basically like the order in which a character 
+# lies in a well ordered set. So if "a" falls at position 97, then it is clearer to 
+# understand why z would fall at 122. Even when you type in "1".ord, you get 49. 
+# So there are characters further in front of even the smallest intergers. 
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count "a"
+=> 4
+
