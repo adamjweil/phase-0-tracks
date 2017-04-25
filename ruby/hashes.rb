7b349b59bee562b917dda4cb3dae196cfbcf4e1b
Adam Weil <adamjweil@gmail.com>
2017-04-25 18:45:12 -0400

    Started the psuedo-code portion of the hashes file, and went thru 5.3 for a couple hours with my pair, gjinny, but still need to finish it

---
+++ ruby/hashes.rb
@@ -0,0 +1,25 @@
+# INTERIOR DESIGNER CLIENT FORM
+# Define a hash
+	# Define the various keys that the hash will contain
+
+# Prompt user for the following information
+	# client name
+	# age
+	# number of children
+	# decor theme
+# Convert this user input into the approptiate data type
+# Make sure the input data is being assigned to a given key within the hash
+# Print the hash back out to the screen after the designer has answered all of the questions
+# Give the designer a change to update any given field
+	# If the designer says 'none'
+	# 	# Skip
+	# If the designer types in:
+	# 	client_name
+	# 		Have the program ask for a new value and update the :client_name key
+	# 	age
+	# 		Have the program ask for a new value and update the :age key
+	# 	number_of_children
+	# 		Have the program ask for a new value and update the :number_of_children key
+	# 	decor_theme
+	# 		Have the program ask for a new value and update the :decor_theme key
+# Print the latest version of the hash and exit the program
