67f29988b529c2366693a2257c5511756cfded4d
Adam Weil <adamjweil@gmail.com>
2017-04-27 15:34:27 -0400

    Updated the finishing touched to 5.2 file

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -2,7 +2,13 @@
 # INTERIOR DESIGNER CLIENT FORM
 # Define a hash
 	# Define the various keys that the hash will contain
-
+		# client name
+		# address
+		# age
+		# email address
+		# phone number
+		# favorite shade of blue
+		# favorite TV show
 # Prompt user for the following information
 	# client name
 	# address
@@ -10,8 +16,7 @@
 	# email address
 	# phone number
 	# favorite shade of blue
-	# number of children
-	# decor theme
+	# favorite TV show
 # Convert this user input into the approptiate data type
 # Make sure the input data is being assigned to a given key within the hash
 # Print the hash back out to the screen after the designer has answered all of the questions
@@ -30,7 +35,7 @@
 # Print the latest version of the hash and exit the program
 # -----> End of Psuedocode <-----
 
-# Initializing/manipulating an array as well as accessing arrays with variables
+# EXPERIMENTING Initializing/manipulating an array as well as accessing arrays with variables
 # address = ['name', 'address', 'email', 'phone', 'fave_blue_shade', 'wallpaper_pref', 'ombre']
 # p address
 # p address.length
@@ -39,6 +44,7 @@
 # p address[3]
 
 # Initializing our hash with all of the variables on our form
+
 form = {
 	"name" => "default_string",
 	"address" => "default_string",
@@ -49,13 +55,11 @@ form = {
 	"fave_tvshow" => "default_string"
 }
 
-
+# Obtaining full name
 puts "What's your full name?"
 form["name"] = gets.chomp.to_s
 
-# puts "What's your address?"
-# form["address"] = gets.chomp.to_s
-
+# Obtaining address
 puts "Enter your address:"
 puts "Street: "
 street = gets.chomp
@@ -67,26 +71,35 @@ puts "Zip Code: "
 zip = gets.chomp
 form["address"] = street + ", " + city + ", " + state + ", " + zip
 
+# Obtaining age
 puts "What's your age?"
 form["age"] = gets.chomp.to_i
 
+# Obtaining email
 puts "What's your email address?"
 form["email"] = gets.chomp.to_s
 
+# Obtaining phone number
 puts "What's your phone number?"
 form["phone"] = gets.chomp.to_i
 
+# Asking favorite shade of blue
 puts "Favorite shade of blue: "
 form["fave_blue_shade"] = gets.chomp.to_s
 
+# Asking what favorite TV show is
 puts "What is your favorite TV show? "
 form["fave_tvshow"] = gets.chomp.to_s
 
-p
+# Printing the form based on the answers to these questions
 p form
 
+# Asking user if there is any updates they would like to make
 puts "Is there any section of the form you would like to update? "
+# Asks user to enter the key to the section they would like to update, or enter none
 puts "If no, enter 'none', if yes, please enter the title of the field you would like to update. "
+
+# If 'none' is entered
 user_input = gets.chomp
 	if user_input == 'none'
 		puts "Final form submission: "
@@ -127,13 +140,13 @@ user_input = gets.chomp
 		puts "Incorrect user input"
 	end
 
-	p
+# Prints the updated form 
 	puts "Latest form submission:"
 	p
 	p form
 
 	end
-
+# End of program
 
 
 
