ab535bde6dbd35589d5254783586f89be5823cdb
Adam Weil <adamjweil@gmail.com>
2017-04-28 16:53:22 -0400

    Submitting the final version of 5.3 Mandatory Pairing: Iteration

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,21 +1,22 @@
 # RELEASE 0
 # 
-# def say_hello
-# 	student1 = "Adam"
-# 	student2 = "Connor"
-# 	student3 = "Jen"
-# 	student4 = "Lindsay"
-# 	p "Status: before"
-# 	puts "Hello students! I wanted to welcome all of my new students to class!"
-# 	yield(student1, student2, student3, student4)
-# 	p "Status: after"
-# end
-# say_hello { |student1, student2, student3, student4| puts "Great to see you, #{student1}, #{student2}, #{student3}, and #{student4}" } 
+def say_hello
+	student1 = "Adam"
+	student2 = "Connor"
+	student3 = "Jen"
+	student4 = "Lindsay"
+	p "Status: before"
+	puts "Hello students! I wanted to welcome all of my new students to class!"
+	yield(student1, student2, student3, student4)
+	p "Status: after"
+end
+say_hello { |student1, student2, student3, student4| puts "Great to see you, #{student1}, #{student2}, #{student3}, and #{student4}" } 
 
 # RELEASE 1
 # 
 # --->Declared an array<-----
 # ->Used .each to modify array data<--
+
 raw_scores = [99, 96, 95, 95, 91, 85]
 curved_scores = []
 
@@ -29,7 +30,7 @@ end
 puts "Curved Scores(after using .each): "
 p curved_scores
 
-# -->Used .map to modify array data<---
+# # -->Used .map to modify array data<---
 raw_scores = [99, 96, 95, 95, 91, 85]
 puts "Original Scores (before using .map!): "
 p raw_scores
@@ -62,30 +63,29 @@ state_capitals.each do |state, capital|
 end
 
 
+# RELEASE 2
+# 
+# Example # 1
+letter_grades = ["a", "b", "b", "c", "d"]
+puts "All Grades: "
+p letter_grades
+letter_grades.delete("b")
+puts "All Grades (minus b's): "
+p letter_grades
+
+# Example # 2
+test_scores = [99, 98, 97, 96, 95, 95, 88, 87, 85]
+puts "Original Test Scores: "
+p test_scores
+test_scores.select! { |num| num.even? }
+puts "Even Test Scores: "
+p test_scores
 
 
-
-
-# numbers = {1 => 'ones', 2 => 'two', 3 => 'three'}
-
-# numbers.each do |digit, word|
-# 	puts "#{digit} is spelled out as #{word}"
-# end
-
-
-
-# numbers = ['1', '2', '3', '4']
-# # modified_names = []
-
-# puts "Original data:"
-# p numbers
-
-
-# modified_numbers = numbers.map do |number|	
-# 	puts number
-# 	number.next
-# end
-
-# puts "After .map call"
-# p numbers
-# p modified_numbers
+# Example # 3
+scores = [97, 95, 93, 92, 89, 88, 86, 85, 82, 81]
+puts "Original Scores: "
+p scores
+scores.delete_if { |score| score < 90 }
+puts "All Scores Better than 90%: "
+p scores
