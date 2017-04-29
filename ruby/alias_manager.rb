cf6dda8b8e198280ed0fe2ca48c3a9267e892d8c
Adam Weil <adamjweil@gmail.com>
2017-04-29 16:53:42 -0400

    Uploading my first version of the 5.5 Solo Challange. Release 0 is almost done, but need to finish release 1 and 2

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,39 @@
+# 5.5 Solo Challenge: Manipulating Strings with Iteration
+# 
+# ----->RELEASE # 0 - Attempt a Tricky Algorithm<------
+# ---> Pseudocode<--
+# Define a method that takes in one parameter as an argument
+		# The data type of this paramter will be a string, and it will be used for entering a spy's 
+		# real full name (first and last)
+	# The first task this method needs to complete is swapping the first and last name (entered as the argument)
+
+
+def spy_name(str)
+
+	split_name = str.split(' ')
+	reverse_name = split_name.reverse
+	spy_name = reverse_name.join(' ')
+	
+	p spy_name
+end
+
+spy_name("Adam Weil")
+
+
+def next_vowel(str)
+vowels = ["a", "e", "i", "o", "u"]
+
+# Write in code that provides the next vowel for every instance of a vowel
+end
+
+
+
+# --->RELEASE # 1<----
+# -->Provide a user interface<---
+# Provide a user interface that lets a user ebter a name and get a fake name back. Let the user do this
+# 	repeatedly until they decide to quit by typing 'quit'. (They might just hit Enter to continue)
+
+
+
+# ----->RELEASE # 2<------
+# --->Store the Aliases<---
