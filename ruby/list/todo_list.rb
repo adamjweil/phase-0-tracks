ca8a6ed010466876092435c56b2a5bdcc16f4e5a
Adam Weil <adamjweil@gmail.com>
2017-05-08 09:15:28 -0400

    Adding commentary regarding identifying the components of an Rspec test, amd the answers to questions raised in Release 2

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -28,3 +28,26 @@ class TodoList
 
 end
 
+# -->Identify the components of an Rspec test:
+# 
+#     RSpec uses the words "describe" and "it" so we can express concepts like a conversation. 
+#     The descibe method creates an ExampleGroup. Within the block passed to describe, you can
+#     declare examples using the it method. 
+#     An example group is a class in which the block passed to describe is evaluated. The blocks passed
+#     to it are exaluated in the context of an instance of that class. 
+# 
+# -------RELEASE: 2--------
+# -->Good references/cheat sheets regarding RSpec:
+#           1.  http://www.rubypigeon.com/posts/rspec-expectations-cheat-sheet/
+# 	2. https://www.anchor.com.au/wp-content/uploads/rspec_cheatsheet_attributed.pdf
+# -->RSpec documentation
+# 	--  http://rspec.info/documentation/
+# 	--  https://relishapp.com/rspec 
+# 	--  http://www.rubydoc.info/gems/rspec-core       (<----Best documentation)
+# -->Other matchers available aside from eq?
+#           -- be_true / be_false / be_nil / be >= x ....
+#           --  start_with / end_with ....
+#           --  be_a <class>  / be_a_kind_of <class>  /  be_an_instance_of <class>
+# 
+
+
