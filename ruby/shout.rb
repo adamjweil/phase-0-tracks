fb010a72fa3eadd1ff4d5627a635c8fcb3e38234
Adam Weil <adamjweil@gmail.com>
2017-05-07 23:18:37 -0400

    Created a standalone module to mixin to my class instances

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,22 +1,38 @@
 module Shout
-	# This is where methods go
 
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def yell_angrily(words)
+		puts "#{words}, grrr!!! :(" 
 	end
 
 
-	def self.yelling_happily(words)
-		words + "!!" + "  ;-)"
+	def yelling_happily(words)
+		puts "#{words}! Woo Hoooo! ;-)"
 	end
 
 end
 
+class Animals
+	include Shout
+end
 
+class Zombies
+	include Shout
+end
 
 # ------>DRIVER CODE<-------
-# -
+# --->Release: 2<-----------
+# p Shout.yelling_happily("yayyyy")
+# p Shout.yell_angrily("boo hoo")
 
-p Shout.yelling_happily("yayyyy")
-p Shout.yell_angrily("boo hoo")
+#__
 
+# ------>DRIVER CODE<---------
+# -------Release: 3<----------
+
+dog = Animals.new
+dog.yell_angrily("Woof Woof")
+dog.yelling_happily("Ruf! Ruf")
+
+walking_dead = Zombies.new
+walking_dead.yell_angrily("GIVE ME HUMAN FLESH NOW!!!!!")
+walking_dead.yelling_happily("YOU TASTE SO GOOD!")
