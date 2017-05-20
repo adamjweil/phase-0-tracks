ef387e2752b3e23fd3075e825688d6dbfba968c1
Adam Weil <adamjweil@gmail.com>
2017-05-20 17:10:40 -0400

    Adding my RubySQLProject.rb file for module 8.5 challange, and adding the corresponding database that I've created, RubySQL.db

---
+++ databases/SQL-Ruby_ComboProject/RubySQLProject.rb
@@ -0,0 +1,14 @@
+# Challange 8.5: Combining SQL and Ruby
+
+# Releases 0-3: Build Something Awesome
+# So far in this course, you've had to satisfy a rubric by reading for detail, asking questions to resolve ambiguity, 
+# and building programs to spec. These are crucial skills for a developer.
+
+# But learning and self-development skills are important, too:
+
+#    --> What do you do with your free time?
+#    --> How do you find ways to push yourself when there's no one around to tell you what to do?
+#    --> How do you find new ideas to explore when we aren't providing them as bullets?
+
+# Your answers to these questions will have a huge impact on your success, and today is your first official opportunity to practice.
+require 'sqlite3'
